02550ac023af0c1d151d35c898496ba2aae2a40b
chenchianstudent <105021007@live.asia.edu.tw>
2016-11-15 16:25:20 +0800

    6

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -15,20 +15,40 @@ System.out.print("½Ð¿é¤Jz");
 double z=scn.nextDouble();
 lcm(x,y,z);
 	}
-public static double lcm(double x,double y,double z){
+public static void lcm(double x,double y,double z){
 	double test=Math.max(x, y);
 	double test2=Math.max(test, z);
-    double	x1=test2%x;
-    double	y1=test2%y;
-    double	z1=test2%z;
-    if(x1!=0&&y1!=0&&z1!=0){
-    int i=1;
-    i++;
-    test*i;	
-    }
     
+    	
+        double	x1=test2%x;
+        double	y1=test2%y;
+        double	z1=test2%z;
+        double sum=test2;
+        
+        if(x1==0&&y1==0&&z1==0){
+        System.out.print(test2);	
+        }else{
+       while(x1!=0||y1!=0||z1!=0){
+    	   
+        	test2=test2+test2;
+            x1=test2%x;
+            y1=test2%y;
+            z1=test2%z;
+            
+            if(x1==0&&y1==0&&z1==0){
+                System.out.print(test2);
+                break;
+        }    	
+            }
+            
+        }
+        	
+    
+    
+    
+   
 		
 		
 	}
 }
-}
+
