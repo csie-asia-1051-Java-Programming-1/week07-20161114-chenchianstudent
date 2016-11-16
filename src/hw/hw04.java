b70ff699cd1ce15d66da58efc058854461f86d25
chenchianstudent <105021007@live.asia.edu.tw>
2016-11-16 10:04:31 +0800

    9

--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -1,11 +1,29 @@
 package hw;
+import java.util.Scanner;
 //寫一遞迴函數，求 m、n 兩數的最大公因數 gcd(m,n)由鍵盤輸入
 
 public class hw04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn=new Scanner (System.in);
+System.out.println("請輸入m");
+int m=scn.nextInt();
+System.out.println("請輸入n");
+int n=scn.nextInt();
+int m1,n1;
+if(n>m){
+m1=n;n1=m;	
+}else{
+m1=m;n1=n;	
+}
+System.out.print(fun1(m1,n1));
 	}
-
+public static int fun1(int m1,int n1){
+	if(n1==0){
+		return m1;
+	}else{
+		return fun1(n1, m1 % n1);
+	}
+}
 }
--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -1,11 +1,23 @@
 package hw;
+import java.util.Scanner;
 //寫一遞迴函數，讓使用者輸入一個長整數，透過遞迴函數運算後得到反向的數字字串並印出。例如:輸入 1234 回傳 4321
 
 public class hw05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner scn=new Scanner(System.in);
+System.out.print("請輸入一長整數");
+long n=scn.nextLong();
 
+fun1(n);
 	}
-
+public static void fun1(long v1){
+	if(v1!=0){
+		
+	fun1(v1%10);
+	}else{
+	
+	}
+}
 }
